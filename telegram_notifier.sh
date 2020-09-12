a59fa5265ff5898f037867bf922b760c417a8e0b
ilia92 <ilia_92@mail.bg>
2020-09-12 22:37:58 +0300

    style fixes

--- telegram_notifier.sh
+++ telegram_notifier.sh
@@ -28,25 +28,40 @@ if ! [ $w_down_notified_count ]; then
 w_down_notified_count=0
 echo "$w_down_name 1" >> $workers_down_file
 
-message=`printf "${message}\nNEW worker down  : $w_line"`
+message="${message}NEW worker down  : ${w_line}\n"
 elif [ $(($w_down_notified_count+1)) -le $counter_trsh ]; then
 
 w_down_notified_count=$(($w_down_notified_count+1))
 sed -i "/${w_down_name}/c${w_down_name}\t$(($w_down_notified_count))" $workers_down_file
 
-message=`printf "${message}\nWorker STILL down: $w_line"`
+message="${message}Worker STILL down: ${w_line}\n"
 fi
 #printf "\n"
 done
 # END Check for new workers down
 
+#printf "==================================\n"
+#printf "$message"
+
+if [[ $message ]]; then
+
 # Arrange NEW first
 message_new=`printf "$message" | grep NEW`
-message_still=`printf "$message" | grep STILL`
-message=`printf "$message_new\n$message_still"`
+message_still=`printf "$message" | grep  STILL`
 # Arrange NEW first
+if [[ $message_new ]] && [[ $message_still ]]; then
+message_new="$message_new\n"
+fi
 
+message_head=`cat "$hash_checker_res_file" | head -4`
+message_tail=`cat "$hash_checker_res_file" | tail -3`
+message=`printf "${message_head}\n${message_new}${message_still}\n${message_tail}"`
+#send message in this case
+printf "Message:\n$message\n"
 
-printf "Message:\n$message\n\n"
+else
+printf "Empty message!\n"
+fi
+
 #printf "file:\n"
 #cat $workers_down_file
