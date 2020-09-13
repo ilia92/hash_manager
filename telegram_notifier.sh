ab9e2c49d75307ad38e9ed12b91991e7153957f0
ilia92 <ilia_92@mail.bg>
2020-09-13 10:18:55 +0300

    telegramer logic only completed

--- telegram_notifier.sh
+++ telegram_notifier.sh
@@ -10,31 +10,29 @@ hash_checker_res=`cat $hash_checker_res_file | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9
 api_key=`cat $DIR/.api_key`
 counter_trsh=5
 
-hash_checker_res_count=`printf "$hash_checker_res" | wc -l`
+workers_down_content=`cat "$workers_down_file"`
 
-message=
-
-# Check for
-for i in `seq 1 $(($hash_checker_res_count+1))`;
+# Check for down workers
+for i in `seq 1 $(($(printf "$hash_checker_res" | wc -l)))`;
 do
 
 w_line=`printf "$hash_checker_res" | sed "$i!d"`
 w_down_name=`printf "$w_line" | awk {'print $1'}`
-w_down_notified_count=`cat $workers_down_file 2> /dev/null | grep "$w_down_name" | awk {'print $2'}`
+w_down_notified_count=`printf "$workers_down_content" | grep "$w_down_name" | awk {'print $2'}`
 
 #printf "\nread: $w_down_name\t$w_down_notified_count\n"
 
-if ! [ $w_down_notified_count ]; then
+if ! [[ "$w_down_notified_count" ]]; then
 w_down_notified_count=0
 echo "$w_down_name 1" >> $workers_down_file
 
-message="${message}NEW worker down  : ${w_line}\n"
+message_new="${message_new}NEW worker down  : ${w_line}\n"
 elif [ $(($w_down_notified_count+1)) -le $counter_trsh ]; then
 
 w_down_notified_count=$(($w_down_notified_count+1))
 sed -i "/${w_down_name}/c${w_down_name}\t$(($w_down_notified_count))" $workers_down_file
 
-message="${message}Worker STILL down: ${w_line}\n"
+message_still="${message_still}Worker STILL down: ${w_line}\n"
 fi
 #printf "\n"
 done
@@ -43,25 +41,44 @@ done
 #printf "==================================\n"
 #printf "$message"
 
-if [[ $message ]]; then
+# Check if miners gone UP
+for j in `seq 1 $(($(printf "$workers_down_content" | wc -l)))`;
+do
+#printf "bla $j\n"
+w_potentially_down=`printf "$workers_down_content" | sed "$j!d" | awk {'print $1'}`
+if_gone_up=`printf "$hash_checker_res" | grep "$w_potentially_down"`
 
-# Arrange NEW first
-message_new=`printf "$message" | grep NEW`
-message_still=`printf "$message" | grep  STILL`
-# Arrange NEW first
+if ! [[ $if_gone_up ]]; then
+message_up="${message_up}Worker gone UP: $w_potentially_down\n"
+sed -i -n "/${w_potentially_down}/!p" $workers_down_file
+fi
+
+done
+# END Check if miners gone UP
+
+#message="${message_new}${message_still}${message_up}"
+
+if [[ $message_new ]] || [[ $message_still ]] || [[ $message_up ]]; then
+
 if [[ $message_new ]] && [[ $message_still ]]; then
 message_new="$message_new\n"
 fi
 
+if [[ $message_new ]] || [[ $message_still ]]; then
 message_head=`cat "$hash_checker_res_file" | head -4`
 message_tail=`cat "$hash_checker_res_file" | tail -3`
-message=`printf "${message_head}\n${message_new}${message_still}\n${message_tail}"`
+message=`printf "${message_head}\n${message_new}${message_still}\n${message_tail}\n"`
+fi
+
+message=`printf "${message}\n${message_up}"`
 #send message in this case
 printf "Message:\n$message\n"
 
 else
 printf "Empty message!\n"
 fi
+
+
 
 #printf "file:\n"
 #cat $workers_down_file
