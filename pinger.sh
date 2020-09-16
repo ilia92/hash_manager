9530556de6ee4e5a2d13d792966bb1e39cbf1f6c
ilia92 <ilia_92@mail.bg>
2020-09-16 05:36:26 +0300

    pinger fix

--- pinger.sh
+++ pinger.sh
@@ -4,11 +4,11 @@ rig=$1
 
 machine=`echo $1 | sed 's|r||g'`
 o=`echo $machine | grep -o '^..'`
-n=`echo $machine | grep -o '..$' `
+n=`echo $machine | grep -o '..$' | sed 's|^0||g'`
 
 #printf "machine: $machine \no: $o\nn: $n\n"
 
-ping -c1 192.168.$o.$n >/dev/null 2>&1 && printf "$rig is UP\n" || printf "$rig still DOWN\n"
-hash=`timeout 1 curl --silent 192.168.$o.$n:3333 | html2text -width 200 | grep "1 minute average\|Average speed" | tail -1 | grep -o -P '(?<=speed \(5 min\): ).*(?=MH/s)'\|'(?<=speed: ).*(?=Mh/s)' | cut -d. -f1`
+ping -c1 192.168.$o.$n >/dev/null 2>&1 && printf "$rig is UP\n" || printf "$rig is DOWN\n"
+hash=`timeout 3 curl --silent 192.168.$o.$n:3333 | html2text -width 200 | grep "1 minute average\|Average speed" | tail -1 | grep -o -P '(?<=speed \(5 min\): ).*(?=MH/s)'\|'(?<=speed: ).*(?=Mh/s)' | cut -d. -f1`
 printf "Hashrate: $hash\n"
 
